e045ce6e7515772027db71f127f51e7e760b94ab
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:25:06 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan12/aritmatika/Pboif210118054Latihan12Aritmatika.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan12.aritmatika;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     * int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
